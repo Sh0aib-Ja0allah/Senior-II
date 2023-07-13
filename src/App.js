27fc904480d540d1a15dc50cbff63470a82d1a98
Sh0aib-Ja0allah <s.ja0allah@outlook.com>
2023-07-14 01:17:46 +0300

    Add the pages files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World!
     </div>
   );
 }
 
 export default App;
+
+// Install react router:
+// npm install --save-dev react-router-dom
+
+// install Material UI:
+// npm install @mui/material @emotion/react @emotion/styled
+
+// install Material UI Icons:
+// npm install @mui/icons-material @mui/material @emotion/styled @emotion/react
